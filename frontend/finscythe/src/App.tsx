25b8c1c4683f64a54e4d5a7f3ad35a633c2227f0
ctjmaxwell <cademaxwell22@gmail.com>
2025-10-25 13:51:07 +0100

    added header and button groups

--- frontend/finscythe/src/App.tsx
+++ frontend/finscythe/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { NavigationMenuDemo } from './components/Header/header'
+import StockTitle from './components/StockChartPanel/stock-title'
+import { ButtonGroupDemo } from './components/StockChartPanel/stock-button-group'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <NavigationMenuDemo />
+      <div className="flex items-center justify-between mt-8">
+        <StockTitle />
+        <ButtonGroupDemo />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
